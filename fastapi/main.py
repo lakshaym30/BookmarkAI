6c7cf9e671fa2d2c08d158a9830e531b6ddccb4b
bukacdan <daniel.bukac99@gmail.com>
2023-06-04 06:05:27 -0700

    wip add extension endpoints fastapi

--- fastapi/main.py
+++ fastapi/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI
-
+from views.chat_view import router as chat_router
 app = FastAPI()
+app.include_router(chat_router)
+
 
 
 @app.get("/")
@@ -11,3 +13,7 @@ async def root():
 @app.get("/hello/{name}")
 async def say_hello(name: str):
     return {"message": f"Hello {name}"}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
